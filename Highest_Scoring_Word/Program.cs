1fc81f4b9bc20ed29934a0959b438b50e7461a80
Barna Hajzer <barna.hajzer@gmail.com>
2018-04-29 22:48:14 +0200

    solve highest_scoring_word kata

---
+++ Highest_Scoring_Word/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Highest_Scoring_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(High("man i need a taxi up to ubud"));
+        }
+
+        public static string High(string s)
+        {
+            var words = s.Split(" ");
+            string maxScoreWord = null;
+            int maxScore = 0;
+            int score = 0;
+            foreach (var w in words) {
+                score = 0;
+                foreach (var c in w) {
+                    score += (int)(c-'a'+1);
+                }
+                if (score > maxScore) {
+                    maxScore = score;
+                    maxScoreWord = w;
+                }
+            }
+
+            return maxScoreWord;
+        }
+    }
+}
