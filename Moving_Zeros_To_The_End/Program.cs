73a3e3299f374ad1aad85be944343017d75ebf67
Barnabás Hajzer <barnabas.hajzer@dealogic.com>
2018-05-02 15:35:33 +0200

    solve Moving Zeros To The End Kata

---
+++ Moving_Zeros_To_The_End/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Moving_Zeros_To_The_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveZeroes(new int[] {1, 2, 0, 1, 0, 1, 0, 3, 0, 1});
+        }
+
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int[] ret = new int[arr.Length];
+            int j = 0;
+            for (int i = 0; i <arr.Length; i++) {
+                if (arr[i]==0) continue;
+                else ret[j++] = arr[i];
+            }
+
+            return ret;
+        }
+    }
+}
