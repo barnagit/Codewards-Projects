97a9f454568983ccf0d57273c15b01041cb55c67
Barnabás Hajzer <barnabas.hajzer@dealogic.com>
2018-05-09 14:32:06 +0200

    Solve Decode the Morse code kata

---
+++ Decode_the_Morse_code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Decode_the_Morse_code
+{
+    class Program
+    {
+        static Dictionary<string,string> MorseCode = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Decode(".... . -.--   .--- ..- -.. ."));
+        }
+
+        public static string Decode(string morseCode)
+        {
+            //MorseCode.Get(".--");
+            StringBuilder sb = new StringBuilder();
+
+            bool inWordSeparationMode = false;
+            string[] letters = morseCode.Split(' ');
+            foreach (String letter in letters) {
+                if (String.IsNullOrEmpty(letter)) {
+                    if (!inWordSeparationMode) {
+                        sb.Append(" ");
+                        inWordSeparationMode = true;
+                    }
+                }
+                else {
+                    inWordSeparationMode = false;
+                    sb.Append(MorseCode[letter]); //use MorseCode.Get(".--") in case of attempting.
+                }
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
