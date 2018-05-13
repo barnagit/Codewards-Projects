c518b64f484a4021e6202b8160f3ee8638141e33
Barna Hajzer <barna.hajzer@gmail.com>
2018-05-12 23:08:54 -0400

    solve Sum Strings as Numbers

---
+++ Sum_Strings_as_Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+namespace Sum_Strings_as_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(sumStrings("123","456"));
+            Console.WriteLine(sumStrings("50095301248058391139327916261","81055900096023504197206408605"));
+        }
+
+        public static string sumStrings(string a, string b)
+        {
+            BigInteger A;
+            BigInteger B;
+
+                if (!BigInteger.TryParse(a, out A)) A = 0;
+                if (!BigInteger.TryParse(b, out B)) B = 0;
+                return (A+B).ToString();            
+        }
+    }
+}
