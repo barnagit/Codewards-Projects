ddbc178e7416030348e021bca98e636074af4636
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-22 21:52:00 +0200

    creating Pyramid_Slide_Down project

---
+++ Pyramid_Slide_Down/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pyramid_Slide_Down
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
