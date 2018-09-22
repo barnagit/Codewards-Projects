0525681351e1d5c534fc359ac16d8043bd6ea039
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-22 17:38:18 +0200

    optimize for performance with caching

--- Twice_linear/Twice_linear.Src/Program.cs
+++ Twice_linear/Twice_linear.Src/Program.cs
@@ -1,59 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Linq;
 
 namespace Twice_linear.Src
 {
 
     public class DoubleLinear 
     {
-        static SortedSet<ulong> L;
-        public static ulong DblLinear (ulong n) 
-        {
-            Console.WriteLine("n:"+n);
-            L = new SortedSet<ulong>();
+        public static ulong[] collector;
 
-            List<ulong> level_old = new List<ulong>();
-            level_old.Add(1);
-            List<ulong> level_new = new List<ulong>();
+        public static int DblLinear(int n) {
+            
+            if (collector != null) {
+                int rr = (int)collector[n];
+                Console.WriteLine(rr);
+                return rr;
+            }
 
-            ulong b;
-            ulong j;
+            int number = 60000;
+            Console.WriteLine("n:"+number);
 
-            ulong s = 0;
-            ulong p = 0;
-            while (s < n) s+=(ulong)Math.Pow(2,p++);
-            s+=(ulong)Math.Pow(2,p++);
+            // what is that level which has the min value greater than the number we are looking for
+            int base_level = (int) Math.Log(number,2) + 1;
+            
+            // what is the highest value in the base level?
+            int top_of_base_level = 0;
+            top_of_base_level += (int)Math.Pow(3,base_level);
+            for (int level_index=0; level_index < base_level; level_index++)
+                top_of_base_level+=(int)Math.Pow(3,level_index);
+            
+            // what is that level lastly changes the sort order of the last level
+            int max_level = base_level;
+            max_level++;
 
-            ulong base_level = (ulong) Math.Log(n,2) + 1;
-            ulong level = base_level;
-            ulong top_of_base_level = (ulong)Math.Pow(3,base_level);
-            for (ulong lc = 0; lc < base_level; lc++) top_of_base_level+=(ulong)Math.Pow(3,lc);
+            ulong[] level_old, level_new;
+            level_old = new ulong[1];
+            level_old[0] = 1;
 
-            while (Math.Pow(2,level+1)-1<top_of_base_level) level++;
+            ulong b,j,l,c;
+            c=0;
 
-            //while (L.Count < s) {
-            for (ulong ll= 0; ll <= level; ll++) {
+            collector = new ulong[(int)Math.Pow(2,max_level+2)];
 
-                level_new = new List<ulong>();
-                foreach(ulong l in level_old) {
-                    b = 2*l+1;
-                    j = 3*l+1;
-                    level_new.Add(b);
-                    level_new.Add(j);
-                    L.Add(b);
-                    L.Add(j);
+            for (int level_index = 0; level_index <= max_level; level_index++) {
+                
+                level_new = new ulong[(int)Math.Pow(2,level_index+1)];
+                l = 0;
+
+                for(int i=0; i < level_old.Length; i++) {
+                    b = 2*level_old[i]+1;
+                    j = 3*level_old[i]+1;
+
+                    level_new[l++] = b;
+                    level_new[l++] = j;
+
+                    collector[c++] = b;
+                    collector[c++] = j;
                 }
 
                 level_old = level_new;
             }
 
-            IEnumerator<ulong> e = L.GetEnumerator();
-            ulong k = 0;
-            while (k++ < n) e.MoveNext();
-            Console.WriteLine("current:"+e.Current);
-            return e.Current;
+            Array.Sort(collector);
+            collector = collector.Distinct().ToArray();
+            ulong r = collector[n];
+            Console.WriteLine(r);
+            return (int)r;
         }
-
     }
 }
--- Twice_linear/Twice_linear.Src/UnitTest1.cs
+++ Twice_linear/Twice_linear.Src/UnitTest1.cs
@@ -9,14 +9,14 @@ namespace Twice_linear.Src
         public void Test1()
         {
             Console.WriteLine("Fixed Tests DblLinear");
-            Assert.Equal(DoubleLinear.DblLinear(10), (ulong)22);
-            Assert.Equal(DoubleLinear.DblLinear(20),(ulong) 57);
-            Assert.Equal(DoubleLinear.DblLinear(30), (ulong)91);
-            Assert.Equal(DoubleLinear.DblLinear(50), (ulong)175);
+            Assert.Equal(DoubleLinear.DblLinear(10), 22);
+            Assert.Equal(DoubleLinear.DblLinear(20), 57);
+            Assert.Equal(DoubleLinear.DblLinear(30),91);
+            Assert.Equal(DoubleLinear.DblLinear(50),175);
 
-            Assert.Equal(DoubleLinear.DblLinear(27723),(ulong)583663);
-            Assert.Equal(DoubleLinear.DblLinear(7279),(ulong)105951);
-            Assert.Equal(DoubleLinear.DblLinear(60000),(ulong)1511311);
+            Assert.Equal(DoubleLinear.DblLinear(27723),583663);
+            Assert.Equal(DoubleLinear.DblLinear(7279),105951);
+            Assert.Equal(DoubleLinear.DblLinear(60000),1511311);
         }
     }
 }
