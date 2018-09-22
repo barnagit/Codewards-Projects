eb9d16c1796e4f0442e2afd5da9a6a3505526c67
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-22 21:56:50 +0200

    add unittests and program's business logic function

--- Pyramid_Slide_Down/Program.cs
+++ Pyramid_Slide_Down/Program.cs
@@ -2,11 +2,16 @@
 
 namespace Pyramid_Slide_Down
 {
-    class Program
+    class PyramidSlideDown
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int LongestSlideDown(int[][] pyramid)
+        {
+            return 0;
         }
     }
 }
