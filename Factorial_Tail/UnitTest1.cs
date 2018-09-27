782328d2fb3dcb94e913713a77c8aee94a35f3f4
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-27 20:05:46 +0200

    Fix the previous fix really. I am sorry

--- Factorial_Tail/UnitTest1.cs
+++ Factorial_Tail/UnitTest1.cs
@@ -6,9 +6,9 @@ namespace Factorial_Tail
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
-        {
-
+        public void Can_Be_Solved_With_Basic_Computations () {
+            Assert.Equal (2, FactorialTail.zeroes (10, 10));
+            Assert.Equal (3, FactorialTail.zeroes (16, 16));
         }
     }
 }
