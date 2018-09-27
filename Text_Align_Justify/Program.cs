24c281051b16afbeb52af2935924510bef280e9d
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-27 06:50:27 +0200

    Create blank project of Text_Align_Justify with a basic unit test.

---
+++ Text_Align_Justify/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Text_Align_Justify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static string NL = Environment.NewLine;
+
+        public static string Justify(string str, int len)
+        {  
+            StringBuilder text = new StringBuilder(str.Length * 2);
+            string[] words = str.Split(' ');
+
+            Queue<string> row = new Queue<string>();
+            int conLen = 0;
+
+            int i = 0;
+            while (i < words.Length) {
+                
+                conLen = 0;
+                row.Clear();
+
+                while (conLen < len) {
+                    if (conLen + words[i].Length + 1 < len) {
+                        conLen += words[i].Length + 1;
+                        row.Enqueue(words[i]);
+                    }
+                    i++;
+                }
+
+                
+            }
+
+            return text.ToString();
+        }
+    }
+}
---
+++ Text_Align_Justify/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Text_Align_Justify
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
