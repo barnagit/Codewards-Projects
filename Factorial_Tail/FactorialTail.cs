25bdf1d02fc70448bfa32512a31849b96b9b9818
Barna Hajzer <barna.hajzer@gmail.com>
2018-09-27 20:03:12 +0200

    Fix missing test method

---
+++ Factorial_Tail/FactorialTail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Factorial_Tail
+{
+    class FactorialTail
+    {
+        public static int zeroes (int b_ase, int number) {
+            int factorial, trailingzeroes = 0;
+            for (factorial = 1; number > 1; factorial *= number--);
+            while (factorial % b_ase == 0) { factorial /= b_ase; ++trailingzeroes; };
+            return trailingzeroes;
+        }
+    }
+}
