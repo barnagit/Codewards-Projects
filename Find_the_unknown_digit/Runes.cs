b058fdbd798d92f33fc00f5d186dbc0d4bf46f51
Barnabás Hajzer <barnabas.hajzer@dealogic.com>
2018-10-05 15:32:57 +0200

    Create Find the Unknown Digit project

---
+++ Find_the_unknown_digit/Runes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Find_the_unknown_digit
+{
+    public class Runes
+    {
+        public static int solveExpression(string expression)
+        {
+            int missingDigit = -1;
+
+            //Write code to determine the missing digit or unknown rune
+            //Expression will always be in the form
+            //(number)[opperator](number)=(number)
+            //Unknown digit will not be the same as any other digits used in expression
+            
+            return missingDigit;
+        }
+    }
+}
