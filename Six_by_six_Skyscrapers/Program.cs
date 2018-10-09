26e8934d15d66828a25710ef52ce5923bb64dece
Barnabás Hajzer <barnabas.hajzer@dealogic.com>
2018-10-09 19:20:02 +0200

    init 6x6 sky scrapers kata

---
+++ Six_by_six_Skyscrapers/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Six_by_six_Skyscrapers
+{
+    class Skyscrapers
+    {
+        public static int[][] SolvePuzzle(int[] clues)
+        {
+            // Start your coding here...
+            return null;
+        }
+    }
+}
